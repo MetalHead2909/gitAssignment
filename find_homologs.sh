a2ca1b5849a6fe17b9a7a2dc0c01258a7481d754
amey agarwal <ameyagarwal10@gmail.com>
2025-09-08 21:28:40 -0400

    edited find_homologs

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,8 +1,10 @@
-query_file=$1
-subject_file=$2
+#!/bin/bash
+
+query_file_protein=$1
+subject_file_nucleotide=$2
 output_file=$3
 
-tblastn -query $query_file -subject $subject_file -task tblastn -outfmt '6 pident length qlen' -out $output_file
+tblastn -query $query_file_protein -subject $subject_file_nucleotide -task tblastn -outfmt '6 pident length qlen' -out $output_file
 
 awk -F'\t' '{if (($1>30) && ((($2)/($3))>0.9)) print $1, $2, $3}' $output_file | wc -l | awk '{print $1}'
 
