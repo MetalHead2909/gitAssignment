b934be6f0651b0b6c371da52e2a2d7ce7911fc76
amey agarwal <ameyagarwal10@gmail.com>
2025-09-08 23:27:08 -0400

    new commit

--- find_homologs.sh
+++ find_homologs.sh
@@ -3,7 +3,6 @@
 query_file_protein=$1
 subject_file_nucleotide=$2
 output_file=$3
-output_value=0
 
 tblastn -query $query_file_protein -subject $subject_file_nucleotide -task tblastn -outfmt '6 pident length qlen' -out $output_file
 
